cf36093b0c703c7364678dba8088b0c8ea5a600d
NM17 <nada.mahmoud@detroitlabs.com>
2019-01-30 10:05:35 -0500

    Initial

---
+++ src/com/company/Main.java
@@ -0,0 +1,29 @@
+package com.company;
+
+import java.lang.reflect.Array;
+import java.security.AlgorithmConstraints;
+import java.util.ArrayList;
+
+public class Main {
+
+    public static void main(String[] args) {
+        ArrayList someApprentices = new ArrayList();
+        someApprentices.add("Nada");
+        someApprentices.add("Omar");
+        someApprentices.add("Christie");
+        someApprentices.add("Jamie");
+        ArrayList moreApprentices = new ArrayList(someApprentices);
+        moreApprentices.add("Brian");
+        moreApprentices.add("Kareem");
+        moreApprentices.add(true);
+
+        System.out.println(moreApprentices);
+
+        ArrayList<String>stringsOnly = new ArrayList<String>();
+        stringsOnly.add("hello");
+        stringsOnly.add("hi");
+        //doesn't work
+        // stringsOnly.add(true);
+
+    }
+}
